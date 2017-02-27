230144ad42950fcb22b1eb1ff4b0bcd31c52b74a
cezannec <cezanne.camacho@gmail.com>
2017-02-26 23:52:36 -0800

    TFragments.06-Exercise-CommunicateBetweenActivities

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -35,6 +35,9 @@ public class AndroidMeActivity extends AppCompatActivity {
         // Only create new fragments when there is no previously saved state
         if(savedInstanceState == null) {
 
+            // TODO (5) Retrieve list index values that were sent through an intent; use them to display the desired Android-Me body part image
+                // Use setListindex(int index) to set the list index for all BodyPartFragments
+
             // Create a new head BodyPartFragment
             BodyPartFragment headFragment = new BodyPartFragment();
 
--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -38,6 +38,13 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
     public void onImageSelected(int position) {
         // Create a Toast that displays the position that was clicked
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
+
+        // TODO (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+
+        // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+
+        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+
     }
 
 }
